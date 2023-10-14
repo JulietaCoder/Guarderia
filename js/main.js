d0241288acce53dc46216d89a94e726dc09854de
Julieta <julieta-andrada@hotmail.com>
2023-10-14 12:45:01 -0300

    Se agrega if/else

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
 alert("¡Bienvenidos!");
 
-prompt("¿Es la primera vez que visita nuestra pagina web?");
-
-
 console.log("Somos una guarderia felina ubicada en Palermo");
 
-const animal = "gatos";
+const animal = "gato";
 let nombre = "Julieta";
 let edad= 30
 let profesion = "Veterinaria";
 
 console.log("Soy" + " " + nombre + " " + "tengo" + " " + edad + " " + "años, soy" + " " + profesion + " " + "y cuento con una guarderia exclusivo de" + " " + animal );
+
+let respuesta = prompt("¿Que animal tiene?");
+
+
+if (respuesta == "gato"){
+    console.log("Puede utilizar el servicio")
+} else {
+    console.log("No puede utilizar el servicio")
+}
+
+
+
+
+
