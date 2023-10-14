bd91e4354dc8154e6a6d12c4948c74c28fc92787
Julieta <julieta-andrada@hotmail.com>
2023-10-14 02:07:51 -0300

    Se inicia proyecto de guarderia

---
+++ js/main.js
@@ -0,0 +1,13 @@
+alert("¡Bienvenidos!");
+
+prompt("¿Es la primera vez que visita nuestra pagina web?");
+
+
+console.log("Somos una guarderia felina ubicada en Palermo");
+
+const animal = "gatos";
+let nombre = "Julieta";
+let edad= 30
+let profesion = "Veterinaria";
+
+console.log("Soy" + " " + nombre + " " + "tengo" + " " + edad + " " + "años, soy" + " " + profesion + " " + "y cuento con una guarderia exclusivo de" + " " + animal );
