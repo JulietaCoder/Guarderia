463c2c41c34cec2cf38adfa6478cf668d2619942
Julieta <julieta-andrada@hotmail.com>
2023-10-16 13:49:59 -0300

    Se incorpora if, else if, else

--- js/main.js
+++ js/main.js
@@ -1,24 +1,36 @@
-alert("¡Bienvenidos!");
+alert("¡Bienvenidos a Guarderia Cuatro Patas!");
 
 console.log("Somos una guarderia felina ubicada en Palermo");
 
 const animal = "gato";
 let nombre = "Julieta";
-let edad= 30
+let edad = 30;
 let profesion = "Veterinaria";
 
 console.log("Soy" + " " + nombre + " " + "tengo" + " " + edad + " " + "años, soy" + " " + profesion + " " + "y cuento con una guarderia exclusivo de" + " " + animal );
 
-let respuesta = prompt("¿Que animal tiene?");
+let respuesta = prompt("¿Cuantos gatos tiene?");
+let precio = 1000;
 
 
-if (respuesta == "gato"){
-    console.log("Puede utilizar el servicio")
-} else {
-    console.log("No puede utilizar el servicio")
+if (respuesta >= 3){
+    precio === 2400
+    console.log("Se aplica un 20% de descuento sobre la estadia")
+} else if (respuesta == 2){
+    precio == 1800
+    console.log("Se aplica un 10% de descuento sobre la estadia")
+} else{
+    precio === 1000
+    console.log("La estadia es de 1000")
 }
 
+let edadGato = prompt("¿Que edad tiene el animal?");
 
+    for(let edadGato=1; edadGato<=10; edad=edadGato+1){
+        console.log("Su animal puede gozar de la estadia");
+        
+    
+    }
 
 
 
