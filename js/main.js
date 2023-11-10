67f544a37ad3347ca57b0048e26b2ae16dd9611f
Julieta <julieta-andrada@hotmail.com>
2023-11-09 23:03:04 -0300

    Se agregan arrays

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 alert("¡Bienvenidos a Guarderia Cuatro Patas!");
 
-console.log("Somos una guarderia felina ubicada en Nuñez");
+console.log("Somos una guarderia felina");
 
 const animal = "gatos";
 let nombre = "Julieta";
@@ -18,7 +18,7 @@ function ubicacion (lugar){
 
 let res = prompt("¿Que edad tiene su gato?");
 
-    while(res <= 15){
+    while(res <= 20){
         console.log("Su animal puede gozar de la estadia")
         break
     }
@@ -64,6 +64,24 @@ function multiplicar (precioNoche , nochesEstadia){
     let resultado = multiplicar(precioNoche , nochesEstadia)
     console.log("El costo total de la estadia es de " + resultado);
 
+
+
+prompt("Elija la sucursal");
+
+const sucursales = ["Nuñez", "Palermo", "Castelar", "Caballito"];
+
+console.log(sucursales);
+console.log(sucursales.includes("Nuñez"));
+
+const detalleSucursales = [
+    {sucu: "Nuñez", direccion: "Montañeses 1653", telefono: 57773478 },
+    {sucu: "Palermo", direccion: "Av Raúl Scalabrini Ortiz 1599", telefono: 57762451 },
+    {sucu: "Castelar", direccion: "Buenos Aires 3489", telefono: 48732901 },
+    {sucu: "Caballito", direccion: "Av. Raúl Scalabrini Ortiz 1599", telefono: 57762451 },
+]
+
+let rdo = detalleSucursales.find( (item)=> item.sucu === "Palermo")
+console.log(rdo);
 
 
 
